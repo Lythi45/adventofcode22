fadd8ff121be890be448c1a6d6ef451debc91cfd
Sabine Wolf <sabine.wolf@ubirch.com>
2022-12-04 18:45:37 +0100

    oneliner for day04

--- day04.py
+++ day04.py
@@ -2,9 +2,7 @@ contains=0
 overlap=0
 for line in open("day04.txt").readlines():
     elf_range1,elf_range2=[set(range(x[0],x[1]+1)) for x in [list(map(int,r.split("-"))) for r in line.strip().split(",")]]
-    #elf_range1,elf_range2=[map(int,r.split("-")) for r in line.strip().split(",")]
     
-    print(elf_range1,elf_range2)
     if elf_range1-elf_range2==set() or elf_range2-elf_range1==set():
         contains+=1
     if elf_range1&elf_range2!=set():
@@ -12,3 +10,8 @@ for line in open("day04.txt").readlines():
 
 print(contains)
 print(overlap)
+
+#One-liner
+print(sum([(lambda x:x[0]<=x[2] and x[1]>=x[3] or x[2]<=x[0] and x[3]>=x[1])(list(map(int,line.strip().replace("-",",").split(",")))) for line in open("day04.txt").readlines()]))
+print(sum([(lambda x:x[2]<=x[1] and x[0]<=x[3])(list(map(int,line.strip().replace("-",",").split(",")))) for line in open("day04.txt").readlines()]))
+
